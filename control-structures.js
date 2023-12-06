207f90acd70c79f9b222e692db5c1d9651e85928
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-05 19:00:49 -0500

    Darnishas Version

---
+++ control-structures.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 5; i++){
+    console.log(`Number ${i}`);
+}
+
+if(name === `Bob`) {
+    console.log(`Hello Bob!`);
+} else {
+    console.log(`You are not Bob`);
+}
+
+
+
+// let name = 'Bob';
+
+// if (name === 'Alice') {
+//     console.log(`Hello Alice!`);
+// } else if (name === 'Bob') {
+//     console.log(`Hello Bob!`);
+// } else {
+//     console.log(`You are not Alice or Bob`);
+// }
